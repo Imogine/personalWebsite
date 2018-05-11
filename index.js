70459abc45103d45bc0b92578aa2ad3c664ab281
imogine <hellogreenwhales@gmail.com>
2018-05-10 19:38:44 -0500

    1.3

--- index.js
+++ index.js
@@ -2,55 +2,69 @@ var modal = document.getElementById('conModal');
 var span = document.getElementsByClassName("close")[0];
 var img = document.getElementsByClassName("img")[0];
 window.onload = function() {
-	modal.style.display = "block";
+    modal.style.display = "block";
 }
 span.onclick = function() {
-	modal.style.display = "none";
+    modal.style.display = "none";
 }
 window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 //Determine Scroll Direction
 var dir = []
 
 function logScrollDirection() {
-	var previous = window.scrollY;
-	window.addEventListener('scroll', function() {
-		window.scrollY > previous ? dir = "down" : dir = "up";
-		previous = window.scrollY;
-	});
-	return dir;
+    var previous = window.scrollY;
+    window.addEventListener('scroll', function() {
+        window.scrollY > previous ? dir = "down" : dir = "up";
+        previous = window.scrollY;
+    });
+    return dir;
 }
 logScrollDirection();
 //Scroll Direction Determined
 
 //Sticky Nav Start
 window.onscroll = function() {
-	logScrollDirection();
-	expAndStick();
+    logScrollDirection();
+    expAndStick();
 };
 var nav = document.getElementById("Nav");
 var sticky = nav.offsetTop;
 
 function expAndStick() {
-	if ((window.pageYOffset >= sticky) && (dir == "down")) {
-		nav.classList.remove("scrollup");
-		nav.classList.add("scrolldown");
-	} else if ((window.pageYOffset <= sticky) && (dir == "up")) {
-		nav.classList.remove("scrolldown");
-		nav.classList.add("scrollup");
-	}
+    if ((window.pageYOffset >= sticky) && (dir == "down")) {
+        nav.classList.remove("scrollup");
+        nav.classList.add("scrolldown");
+    } else if ((window.pageYOffset <= sticky) && (dir == "up")) {
+        nav.classList.remove("scrolldown");
+        nav.classList.add("scrollup");
+    }
 }
 //Sticky Nav End
 
-img.onclick = function() {
-	div.style.width = "800px";
+//Gallery Begin -
+// 1.) Click Image
+// 2.) Image is displayed on right,
+
+var artR = document.getElementById('artr');
+
+
+function image(x) {
+    var y = ["art/hh1.jpg", "art/milosz.jpg", "art/bruce.png", "art/dog.png", "art/hh2.png", "art/willow.png"]
+    artR.style.background = "url(" + y[x] + ")";
+    artR.style.backgroundPosition = "center";
+    artR.style.backgroundRepeat = "no-repeat";
+    artR.style.backgroundSize = "contain";
 }
-window.onclick = function(event) {
-	if (event.target == img) {
-		div.style.display = "none";
-	}
+
+function mimage(x) {
+    var y = ["art/driveinmockup.png", "art/wp.png"]
+    mDisp.style.background = "url(" + y[x] + ")";
+    mDisp.style.backgroundPosition = "center";
+    mDisp.style.backgroundRepeat = "no-repeat";
+    mDisp.style.backgroundSize = "contain";
 }
